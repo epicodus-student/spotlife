21a9369e514c3d50c46ad5e644f415d7fd901c93
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-04-07 16:56:02 -0700

    Break on Start

--- ember/app/app.js
+++ ember/app/app.js
@@ -13,4 +13,7 @@ var App = Ember.Application.extend({
 
 loadInitializers(App, config.modulePrefix);
 
+
+
+loadInitializers(App, 'rails-csrf');
 export default App;
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
+
+  namespace :api do
+    get :csrf, to: 'csrf#index'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
