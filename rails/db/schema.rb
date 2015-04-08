8974f603967e7b5a9b155b702a5bf17f44af5965
Alex Beeken <aeb242@gmail.com>
2015-04-08 11:14:37 -0700

    Have Posts Table

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407222220) do
+ActiveRecord::Schema.define(version: 20150408181004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string  "body"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
